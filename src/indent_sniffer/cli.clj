2a357e24db71ab97799e14b9ba809d32cac4fa16
Korny Sietsma <korny@sietsma.com>
2016-04-26 08:24:12 +0100

    added recursive directory scanning, check for hidden files, limit to specific extensions

--- src/indent_sniffer/cli.clj
+++ src/indent_sniffer/cli.clj
@@ -7,22 +7,25 @@
     [clojure.string :as string]
     [me.raynes.fs :as fs]
     [clojure.pprint :refer [pprint]])
-  (:gen-class))
+  (:gen-class)
+  (:import (java.io File)))
 
 (def cli-options
   [["-o" "--output filename" "select an output file name (default is STDOUT)"]
+   ["-e" "--extensions list-of-file-extensions" "list of file extensions to scan - case insensitive, comma separated"]
+   ["-i" "--hidden" "include hidden directories and files"]
    ["-h" "--help"]])
 
 (defn usage [options-summary]
   (->> ["Utilities for slurping and combining source code metrics"
         ""
-        "Usage: indent-sniff [options] files"
+        "Usage: indent-sniff [options] files or directories"
         ""
         "Options:"
         options-summary
         ""
-        "Files:"
-        "Files is one or more files - if you run zsh use a glob here like **/*.clj to traverse the filesystem"
+        "Files or directories:"
+        "individual files can be named, or you can specify directories to be recursively scanned (use other options to filter)"
         ""]
        (string/join \newline)))
 
@@ -37,6 +40,32 @@
 
 (def default-indents [0 2 3 4])
 
+(defn file-ends-with [file extension]
+  (-> file
+         .getName
+         .toLowerCase
+         (.endsWith extension)))
+
+(defn filter-files [{:keys [hidden extensions]} file]
+  (if (and (not hidden) (.isHidden file))
+    false
+    (if (nil? extensions)
+      true
+      (if (and (.isFile file) (not-any? (partial file-ends-with file) extensions))
+        false
+        true))))
+
+
+(defn scan-file-or-files [options ^File file]
+  (if (.isFile file)
+    (let [rdr (io/reader file)
+          out (scanner/best-file-line-stats rdr default-indents)]
+      {:filename (.getPath file)
+       :data     {:indents out}})
+    (->> (.listFiles file)
+         (filter (partial filter-files options))
+         (map (partial scan-file-or-files options)))))
+
 (defn -main [& args]
   (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)]
     (cond
@@ -45,14 +74,16 @@
       errors (exit 1 (error-msg errors)))
     (let [out-file (if (:output options)
                      (io/writer (:output options))
-                     *out*)]
+                     *out*)
+          extensions (if (:extensions options)
+                       (map string/lower-case (string/split (:extensions options) #","))
+                       nil)
+          hidden (:hidden options)]
       (try
         (->
-          (for [file arguments]
-            (let [rdr (io/reader file)
-                  out (scanner/best-file-line-stats rdr default-indents)]
-              {:filename file
-               :data     {:indents out}}))
+          (for [file (map io/file arguments)]
+            (scan-file-or-files {:extensions extensions :hidden hidden} file))
+          flatten
           (cheshire/generate-stream out-file {:pretty true}))
         (finally
           (if (:output options)
